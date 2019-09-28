52565360d7762a11768a12c7a4f03c0721868386
nwy140 <nwy140@gmail.com>
2019-09-27 20:38:10 -0600

    Update EnemySpawner.cs

--- SiezeTheMemes/Assets/Enemies/Scripts/TowerDefenseLogic/EnemySpawner.cs
+++ SiezeTheMemes/Assets/Enemies/Scripts/TowerDefenseLogic/EnemySpawner.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -8,4 +9,18 @@ public class EnemySpawner : MonoBehaviour
         // spawn enem1ies by number
         // add each enemy to the array
             // activate each enemy one by one 
+    public GameObject spawnPoint;
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            GameObject spawnObj = CnvMechObjPoolSgtMan.instance.GetPooledObjectRandom();
+            spawnObj.transform.position = spawnPoint.transform.position;
+            spawnObj.GetComponent<MechCharStatHP>().currentHP = 1000;
+            
+            spawnObj.SetActive(true);
+            /// heal 
+        }
+    }
 }
