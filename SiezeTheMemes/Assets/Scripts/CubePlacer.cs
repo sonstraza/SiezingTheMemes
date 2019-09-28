5302352b4c7c631a9a5f928ccf2ad5f9a9cd7abd
Brigham777 <ighambray@outlook.com>
2019-09-27 21:17:31 -0600

    update scrpts

---
+++ SiezeTheMemes/Assets/Scripts/CubePlacer.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class CubePlacer : MonoBehaviour
+{
+    public GameObject[] TowerArray = new GameObject[3];
+
+    private TowerGrid grid;
+    
+
+
+
+
+    private void Awake()
+    {
+        grid = FindObjectOfType<TowerGrid>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            RaycastHit hitInfo;
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+            if (Physics.Raycast(ray, out hitInfo))
+            {
+                PlaceCubeNear(hitInfo.point);
+            }
+        }
+    }
+
+    private void PlaceCubeNear(Vector3 clickPoint)
+    {
+        var finalPosition = grid.GetNearestPointOnGrid(clickPoint);
+        Instantiate(TowerArray[0], finalPosition, Quaternion.identity);
+
+    }
+}
